364745258684f1aed32fddfbdbb0bb7c8ec9e325
nrshakib <nrshakib007@gmail.com>
2025-09-06 11:57:12 +0600

    Project Set Up

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
+"use client";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const steps = [
+  { id: 0, label: "Personal" },
+  { id: 1, label: "Job" },
+  { id: 2, label: "Skills" },
+  { id: 3, label: "Emergency" },
+  { id: 4, label: "Review" },
+];
+
 export default function Home() {
+  const [step, setStep] = useState(0);
+  const form = useForm({
+    mode: "onBlur",
+  });
+
   return (
     <div>
-      <p className="text-lg text-red-400">Hello</p>
+      <p>Form</p>
     </div>
   );
 }
